5d839057329e2c8762202c698874e08a2c66238a
nihae28 <nepuri28usa@gmail.com>
2022-05-02 10:27:22 -0500

    Added logout in header component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,7 +3,7 @@ import Auth from '../services/Auth';
 
 let auth = new Auth();
 
-export default class Navbar extends Component {
+export default class HeaderComponent extends Component {
 
     constructor(){
         super();
