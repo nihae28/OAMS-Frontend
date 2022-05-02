b98664eaa53807b5baa60bdbcf9b329eb5cee0e6
Pachipulusu Raghuveer <ptnbvr@gmail.com>
2022-05-02 12:54:14 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,8 @@
 import { Component } from "react";
 import './Home.css';
-export default class TrafficController extends Component {
+export default class Home extends Component {
 
     render() {
-        console.log(this.props.match.params.ssn)
         return (
             <div class="section-center">
 				<h1 class="mb-0">Online Airport System,</h1>
